cc94cfdcc79e27397e99b3bc2ec3541e3d323b59
luka <lukabura89@gmail.com>
2022-12-20 14:05:08 +0100

    modified class dice and implementing roll dice in function turn

--- src/dice.cpp
+++ src/dice.cpp
@@ -5,15 +5,34 @@
 Dice::Dice():
     m_dice1(0), m_dice2(0) {}
 
-void Dice::set_dice1(int num) { m_dice1 = num; }
+void Dice::set_button_is_clicked(bool button_clicked){
+    m_button_is_clicked = button_clicked;
+}
 
-void Dice::set_dice2(int num) { m_dice2 = num; }
 
-int Dice::get_dice1() const{ return m_dice1; }
+void Dice::set_dice1(int num) {
+    m_dice1 = num;
+}
 
-int Dice::get_dice2() const{ return m_dice2; }
+void Dice::set_dice2(int num) {
+    m_dice2 = num;
+}
 
-int Dice::get_dice_sum() const { return m_dice1 + m_dice2; }
+int Dice::get_dice1() const{
+    return m_dice1;
+}
+
+int Dice::get_dice2() const{
+    return m_dice2;
+}
+
+int Dice::get_dice_sum() const {
+    return m_dice1 + m_dice2;
+}
+
+bool Dice::get_button_clicked() const {
+    return m_button_is_clicked;
+}
 
 void Dice::roll_dice() {
     srand(time(0));
--- src/dice.h
+++ src/dice.h
@@ -7,15 +7,21 @@ class Dice
 private:
     int m_dice1;
     int m_dice2;
+    bool m_button_is_clicked = false;
+
 public:
     Dice();
     Dice(const Dice &) = delete;    //destructor
 
+    void set_button_is_clicked(bool button_clicked);
     void set_dice1(int num);
     void set_dice2(int num);
+
     int get_dice1() const;
     int get_dice2() const;
     int get_dice_sum() const;
+    bool get_button_clicked() const;
+
     void roll_dice();
 };
 
