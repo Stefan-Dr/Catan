f3324b7fb5707ff4f77d53c194dda2dce442fa0a
VukoticLuka <lukavukotic811@gmail..com>
2022-12-28 01:32:27 +0100

    checking for a node if  its neighbors are occupied

--- src/board.cpp
+++ src/board.cpp
@@ -642,8 +642,17 @@ void Board::mousePressEvent(QGraphicsSceneMouseEvent *event){
     if(m_setHouse && (itemAt(event->scenePos(),QTransform())->type()==1)){
         Redraw();
         GUI_Node *gui_node = dynamic_cast<GUI_Node*>(itemAt(event->scenePos(),QTransform()));
+        //provera za node da li na susednim cvorovima ima izgradjenih obejkata
+        auto node_neighbours = gui_node->Node_get()->get_neighbours();
+        int neighbours_are_occupied = 0;
+        for(auto &neighbour : node_neighbours){
+            if(m_nodes[neighbour-1]->get_is_house_built() || m_nodes[neighbour-1]->get_is_city_built()){
+                neighbours_are_occupied = 1;
+                break;
+            }
+        }
         //prvo proveravamo da li nema nicega sagradjenog da mozemo da sagradimo kucu
-        if (!gui_node->get_is_house_built()){
+        if (!gui_node->get_is_house_built() && neighbours_are_occupied == 0){
             gui_node->setBrush(QBrush(getCurrColor()));
             gui_node->set_is_house_built(true);
             //gui_node->getNode()->set_owner()
