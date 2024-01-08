e5224569c0befa17bb735d252480d2714049da9f
Stefan Drljevic <drljevicstefan@gmail.com>
2024-01-09 00:54:18 +0100

    cmake

--- src/build/CMakeFiles/Catan.dir/DependInfo.cmake
+++ src/build/CMakeFiles/Catan.dir/DependInfo.cmake
@@ -1,45 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/Catan_autogen/mocs_compilation.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/Catan_autogen/mocs_compilation.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/bank.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/bank.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/board.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/board.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/dice.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/dice.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/field.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/field.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/game.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/game.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/gui_node.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/gui_node.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/gui_road.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/gui_road.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/main.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/main.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/mainwindow.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/mainwindow.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/node.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/node.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/player.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/player.cpp.o"
-  "/home/snele/Desktop/azrs-Projekat/06-catan/src/sources/road.cpp" "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/CMakeFiles/Catan.dir/sources/road.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "QT_CORE_LIB"
-  "QT_GUI_LIB"
-  "QT_NETWORK_LIB"
-  "QT_NO_DEBUG"
-  "QT_WIDGETS_LIB"
   )
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "."
-  "../"
-  "Catan_autogen/include"
-  "/usr/include/x86_64-linux-gnu/qt5"
-  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
-  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
-  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
-  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
-  "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stefan/Desktop/Catan/06-catan/src/build/Catan_autogen/mocs_compilation.cpp" "CMakeFiles/Catan.dir/Catan_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/Catan.dir/Catan_autogen/mocs_compilation.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/bank.cpp" "CMakeFiles/Catan.dir/sources/bank.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/bank.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/board.cpp" "CMakeFiles/Catan.dir/sources/board.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/board.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/dice.cpp" "CMakeFiles/Catan.dir/sources/dice.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/dice.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/field.cpp" "CMakeFiles/Catan.dir/sources/field.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/field.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/game.cpp" "CMakeFiles/Catan.dir/sources/game.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/game.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/gui_node.cpp" "CMakeFiles/Catan.dir/sources/gui_node.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/gui_node.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/gui_road.cpp" "CMakeFiles/Catan.dir/sources/gui_road.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/gui_road.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/main.cpp" "CMakeFiles/Catan.dir/sources/main.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/main.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/mainwindow.cpp" "CMakeFiles/Catan.dir/sources/mainwindow.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/mainwindow.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/node.cpp" "CMakeFiles/Catan.dir/sources/node.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/node.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/player.cpp" "CMakeFiles/Catan.dir/sources/player.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/player.cpp.o.d"
+  "/home/stefan/Desktop/Catan/06-catan/src/sources/road.cpp" "CMakeFiles/Catan.dir/sources/road.cpp.o" "gcc" "CMakeFiles/Catan.dir/sources/road.cpp.o.d"
   )
 
 # Targets to which this target links.
--- src/build/cmake_install.cmake
+++ src/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/snele/Desktop/azrs-Projekat/06-catan/src
+# Install script for directory: /home/stefan/Desktop/Catan/06-catan/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,6 +37,11 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -45,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/snele/Desktop/azrs-Projekat/06-catan/src/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/stefan/Desktop/Catan/06-catan/src/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
