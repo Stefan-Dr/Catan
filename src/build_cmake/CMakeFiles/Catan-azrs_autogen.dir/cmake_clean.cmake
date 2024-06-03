7219c263c99c6edffa8a166b58285d7848d6422f
luka_120_2019 <lukavukotic811@gmail..com>
2024-06-03 22:56:49 +0200

    We deleted all files from build_cmake so we can more easily push to our remote repository

--- src/build_cmake/CMakeFiles/Catan-azrs_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Catan-azrs_autogen"
-  "Catan-azrs_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Catan-azrs_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
